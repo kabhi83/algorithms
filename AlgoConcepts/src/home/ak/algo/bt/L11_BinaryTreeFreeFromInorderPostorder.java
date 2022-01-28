e1a66682ca8ae7f0205b63d6d47f4138d8b202d8
Abhishek Kundu <kundu.abhishek83@gmail.com>
2022-01-29 00:38:24 +0530

    New binary tree problems

---
+++ AlgoConcepts/src/home/ak/algo/bt/L11_BinaryTreeFreeFromInorderPostorder.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package home.ak.algo.bt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import home.ak.algo.bt.BinaryTree.Node;
+
+/**
+ * @author kundu
+ *
+ */
+public class L11_BinaryTreeFreeFromInorderPostorder {
+
+	public static Node buildTree(int[] postorder, int[] inorder) {
+		Map<Integer, Integer> inMap = new HashMap<>();
+		for (int i = 0; i < inorder.length; i++) {
+			inMap.put(inorder[i], i);
+		}
+		Node root = buildTree(postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1, inMap);
+		return root;
+	}
+
+	private static Node buildTree(int[] postorder, int postStart, int postEnd, int[] inorder, int inStart, int inEnd,
+			Map<Integer, Integer> inMap) {
+		if (postStart > postEnd || inStart > inEnd) {
+			return null;
+		}
+		Node root = new Node(postorder[postEnd]);
+		int inorderRootIdx = inMap.get(root.data);
+		int numsLeft = inorderRootIdx - inStart;
+		root.left = buildTree(postorder, postStart, postStart + numsLeft - 1, inorder, inStart, inorderRootIdx - 1, inMap);
+		root.right = buildTree(postorder, postStart + numsLeft, postEnd - 1, inorder, inorderRootIdx + 1, inEnd, inMap);
+
+		return root;
+	}
+
+	public static void main(String[] args) {
+		int[] postorder = { 1, 4, 7, 6, 3, 13, 14, 10, 8 };
+		int[] inorder = { 1, 3, 4, 6, 7, 8, 10, 13, 14 };
+
+		
+		Node root = buildTree(postorder, inorder);
+		System.out.println(root.data);
+		System.out.println(root.left.data);
+		System.out.println(root.right.data);
+		System.out.println(root.left.left.data);
+		System.out.println(root.left.right.data);
+		System.out.println(root.right.right.data);
+	}
+
+}
---
+++ AlgoConcepts/src/home/ak/algo/bt/L12_BSTLowestCommonAncestor.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package home.ak.algo.bt;
+
+import home.ak.algo.bt.BinaryTree.Node;
+
+/**
+ * @author kundu
+ *
+ */
+public class L12_BSTLowestCommonAncestor {
+
+	public static Node lowestCommonAncestor(Node root, Node p, Node q) {
+		if (null == root) {
+			return null;
+		}
+
+		int curr = root.data;
+		if (curr > p.data && curr > q.data) {
+			// Search left subtree
+			return lowestCommonAncestor(root.left, p, q);
+		}
+		if (curr < p.data && curr < q.data) {
+			// Search right subtree
+			return lowestCommonAncestor(root.right, p, q);
+		}
+		// If we cannot compare both the nodes then the current node is LCA
+		// This applies when 1 node is left subtree and 1 node in right subtree
+		return root;
+	}
+
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+		tree.add(8);
+		tree.add(3);
+		tree.add(6);
+		tree.add(10);
+		tree.add(4);
+		tree.add(7);
+		tree.add(1);
+		tree.add(14);
+		tree.add(13);
+
+		Node root = tree.root;
+		Node p = root.left.left;
+		Node q = root.left.right;
+		
+		System.out.println(lowestCommonAncestor(root, p, q).data);
+	}
+
+}
